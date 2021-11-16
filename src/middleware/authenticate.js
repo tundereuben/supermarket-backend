b6454e4a19d9fdfa225d996f4d63b100be88dede
tundereuben@yahoo.com <tunde.ogunjimi>
2021-11-16 17:17:01 +0100

    Added user routes

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,10 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const auth = async (req, res, next) => {
-  try {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-      next()
-  } catch (e) {
-      res.status(401).send({ error: 'An error occurred!'})
-  }
-};
+    if (!user) {
+        throw new Error()
+    }
 
-module.exports = auth;
+    req.token = token
+    req.user = user
+    next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
