ef6967ae31bf9619d99f87338da99c3b611fac43
tundereuben <tundereuben@yahoo.com>
2021-11-20 11:05:19 +0100

    Set configurations

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,11 +9,11 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        sendWelcomeEmail(user.email, user.firstName)
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
+        sendWelcomeEmail(user.email, user.name);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
-        res.status(400).send(e)
+        res.status(400).send(e);
     }
 });
 
